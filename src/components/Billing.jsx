4c1940e43a46030a6a8f8aacb66651eeeb2999ce
sachikanta15 <sachikantaraulchintu@gmail.com>
2024-04-20 02:18:20 +0530

    added sections

---
+++ src/components/Billing.jsx
@@ -0,0 +1,43 @@
+import styles, { layout } from "@/styles/styles";
+import { apple, bill, detailTransactions, google,widgets } from "../../public/assets";
+import Image from "next/image";
+
+const Billing = () => (
+  <section
+    id="product"
+    className="flex md:flex-row flex-col-reverse sm:py-16 py-6"
+  >
+    <div className=" flex justify-center items-center md:mr-0 mr-0 md:mt-0 mt-5 relative">
+    <Image src={widgets} alt="card" className="w-[50%] h-[100%] rounded-xl object-contain"/>
+
+      {/* gradient start */}
+      <div className="absolute z-[3] -left-1/2 top-0 w-[50%] h-[50%] rounded-full white__gradient" />
+      <div className="absolute z-[0] w-[50%] h-[50%] -left-1/2 bottom-0 rounded-full pink__gradient" />
+      {/* gradient end */}
+    </div>
+
+    <div className="flex-1 flex justify-center items-start flex-col">
+      <h2 className="font-poppins font-semibold xs:text-[48px] text-[40px] text-black xs:leading-[76.8px] leading-[66.8px] w-full">
+     Financial Oversight:  <br className="sm:block hidden" /> Track Every Transaction with Widgets
+      </h2>
+      <p className=" font-poppins font-normal text-dimblack text-[18px] leading-[30.8px] max-w-[470px] mt-5">
+      Stay on top of your finances with customizable widgets for all transactions, incoming, and outgoing, keeping you in control at a glance with ExpenseTracker.money
+      </p>
+
+      {/* <div className="flex flex-row flex-wrap sm:mt-10 mt-6">
+        <Image
+          src={apple}
+          alt="google_play"
+          className="w-[128.86px] h-[42.05px] object-contain mr-5 cursor-pointer"
+        />
+        <Image
+          src={google}
+          alt="google_play"
+          className="w-[144.17px] h-[43.08px] object-contain cursor-pointer"
+        />
+      </div> */}
+    </div>
+  </section>
+);
+
+export default Billing;
