3c8ea79cfba8c94428bf4450af052de671e28f2a
sachikanta15 <sachikantaraulchintu@gmail.com>
2024-04-20 02:07:52 +0530

    added assets

---
+++ public/assets/index.js
@@ -0,0 +1,38 @@
+
+
+import logo from "./logo.jpeg";
+import send from "./Send.svg";
+import shield from "./Shield.svg";
+import star from "./Star.svg";
+import menu from "./menu.svg";
+import close from "./close.svg";
+import google from "./google.svg";
+import apple from "./apple.svg";
+import arrowUp from "./arrow-up.svg";
+import facebook from "./facebook.svg";
+import instagram from "./instagram.svg";
+import linkedin from "./linkedin.svg";
+import twitter from "./twitter.svg";
+import allTransactions from "./allTransaction.jpeg";
+import detailTransactions from "./detailTransactions.jpeg";
+import widgets from "./widgets.jpeg"
+import analytics from "./analytics.svg"
+export {
+  widgets,
+  detailTransactions,
+  allTransactions,
+  logo,
+  send,
+  shield,
+  star,
+  menu,
+  close,
+  google,
+  apple,
+  arrowUp,
+  facebook,
+  instagram,
+  linkedin,
+  twitter,
+  analytics,
+};
